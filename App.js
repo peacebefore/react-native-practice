430be3b9143bc4741c617fc5379604e8f508044b
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-18 10:45:44 -0700

    adding safeareaview and setting up homescreen layout

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Statusbar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style = {{ flex: 1, marginTop: Statusbar.currentHeight }}>
+        <View style = {{ padding: 16, backgroundColor: 'green' }} >
+          <Text> Search </Text>
+        </View>
+        <View style = {{ flex: 1, padding: 16, backgroundColor: 'blue' }} >
+          <Text> List </Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
