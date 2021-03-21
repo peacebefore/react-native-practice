8d61fb97ecf85b332045674aefb84c9c6a01f9c9
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-20 18:34:44 -0700

    adding theme

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, Stylesheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
@@ -14,13 +14,16 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Title = styled(Text)`
   padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
     icon,
-    photos = ["https://unsplash.com/photos/uzaYCt4tOoE"],
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
@@ -28,12 +31,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <RestaurantCard elevation={5} style={styles.card}>
-      <RestaurantCardCover
-        key={name}
-        style={styles.cover}
-        source={{ uri: photos[0] }}
-      />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
     </RestaurantCard>
   );
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,23 +1,23 @@
 import React from "react";
-import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, Text, View, SafeAreaView } from "react-native";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  margin-top: ${StatusBar.currentHeight}px;
 `;
 
 const SearchContainer = styled.View`
   padding: 16px;
-  background-color: green;
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
   padding: 16px;
+  background-color: blue;
 `;
 
 export const RestaurantsScreen = () => (
