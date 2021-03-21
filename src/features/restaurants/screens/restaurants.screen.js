3560cb7d92dbabe404add15d22acdf60d80813e2
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-20 21:31:21 -0700

    added spacer component

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,17 +7,16 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.spacing.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: ${(props) => props.theme.spacing.space[3]};
-  background-color: ${(props) => props.theme.colors.brand.primary};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
